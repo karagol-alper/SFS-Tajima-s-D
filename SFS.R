25d97b9bd7b71ba3d086081a98f5f024f539ca81
Alper Karagöl <149246629+karagol-alper@users.noreply.github.com>
2024-07-30 16:37:23 +0300

    Update and rename SFS and TajimaD.R to SFS.R

--- SFS.R
+++ SFS.R
@@ -47,33 +47,3 @@ SFS <- compute_SFS(alignment)
 
 # Output the SFS
 print(SFS)
-
-# The function for computeing TajimasD values
-TajimasD <- function(sfs){
-    #For the computed SFS
-    n <- length(sfs) + 1
-    ss <- sum(sfs)
-    
-    a1 <- sum(1 / seq_len(n-1))
-    a2 <- sum(1 / seq_len(n-1)^2)
-    
-    b1 <- (n + 1) / (3 * (n - 1))
-    b2 <- 2 * (n^2 + n + 3)/(9 * n * (n - 1))
-    
-    c1 <- b1 - 1/a1
-    c2 <- b2 - (n + 2)/(a1 * n) + a2 / a1^2
-    
-    e1 <- c1 / a1
-    e2 <- c2 / (a1^2 + a2)
-    
-    Vd <- e1 * ss + e2 * ss * (ss - 1) 
-    
-    theta_pi <- sum(2 * seq_len(n-1) * (n - seq_len(n-1)) * sfs)/(n*(n-1))
-    theta_w <- ss / a1
-    res <- (theta_pi - theta_w) / sqrt(Vd)
-    return(res)
-}
-
-#Compute TajimaD
-TajimasD(SFS)
-
